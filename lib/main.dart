2ac8f727652b5b1395c1ad2e4b19f6c48fe20d78
ahmedsalemgithub <ahmedsalem.business@gmail.com>
2019-09-10 20:00:36 +0200

    init

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter_web/material.dart';
+import 'package:thecoder/main_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'The Coder onLine',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+   
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlatButton(
+              child: Text("test the 21 Cards Game"),
+              onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (BuildContext context){return MainScreen();}));
+              },
+            )
+          ],
+        ),
+      ), 
+    );
+  }
+}
